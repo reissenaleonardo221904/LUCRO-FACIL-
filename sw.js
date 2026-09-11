63d1176be5c08f6d067a33ede425563395f3f7e1
reissenaleonardo221904 <reissenaleonardo2@gmail.com>
2026-09-11 07:33:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,96 @@
-const CACHE = 'lucro-facil-v1-4';
-const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'lucro-facil-v1-5';
+
+const CORE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon.svg'
+];
 
 self.addEventListener('install', function(event) {
+
   event.waitUntil(
+
     caches.open(CACHE).then(function(cache) {
+
       return cache.addAll(CORE);
+
     })
+
   );
 
   self.skipWaiting();
+
 });
+
 
 self.addEventListener('activate', function(event) {
+
   event.waitUntil(
+
     caches.keys().then(function(keys) {
+
       return Promise.all(
+
         keys
           .filter(function(key) {
+
             return key !== CACHE;
+
           })
           .map(function(key) {
+
             return caches.delete(key);
+
           })
+
       );
+
     }).then(function() {
+
       return self.clients.claim();
+
     })
+
   );
+
 });
 
+
 self.addEventListener('fetch', function(event) {
+
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
+
     fetch(event.request)
+
       .then(function(response) {
+
         var copy = response.clone();
 
         caches.open(CACHE).then(function(cache) {
+
           cache.put(event.request, copy);
+
         });
 
         return response;
+
       })
+
       .catch(function() {
-        return caches.match(event.request).then(function(cached) {
-          return cached || caches.match('./index.html');
-        });
+
+        return caches.match(event.request)
+
+          .then(function(cached) {
+
+            return cached || caches.match('./index.html');
+
+          });
+
       })
+
   );
+
 });
